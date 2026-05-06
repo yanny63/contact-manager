aa5b652226bd2e7940c2be0f51402a21f144c18b
yanny63 <contact.yanny0@gmail.com>
2026-05-07 00:00:43 +0200

    feat: add login

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,9 +1,56 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+
+    const navigate = useNavigate()
+
+    async function login(e) {
+        e.preventDefault()
+        const data = new FormData(e.target)
+        const user = Object.fromEntries(data)
+        console.log(user)
+
+        try {
+            const res = await fetch('/login', {
+                method: "POST",
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(user)
+            })
+            if (!res.ok) {
+                throw new Error(res.details)
+            }
+            const data = await res.json()
+            localStorage.setItem('token', data.access_token)
+            navigate('/')
+        }
+        catch (err) {
+            console.log(`Error - ${err}`)
+        }
+    }
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="login-main">
+            <div className="login-form-outer-container">
+                <div>
+                    <h1>Zaloguj się</h1>
+                    <p>Aby móc czatować ze swoimi znajomymi!</p>
+                </div>
+                <div className="isLine"></div>
+                <form onSubmit={login} className="login-form">
+                    <div className="login-form-inner-container">
+                        <label htmlFor='phone'>Numer Telefonu</label>
+                        <input className="form-input" name='phone' id='phone' type='number' placeholder='111 222 333' />
+                    </div>
+                    <div className="login-form-inner-container">
+                        <label htmlFor="password">Hasło</label>
+                        <input className="form-input" name='password' type="password" id='password' placeholder='••••••••••••' />
+                    </div>
+                    <button className="form-button">Zaloguj się</button>
+                </form>
+            </div>
+            <div className="login-form-outer-container">
+
+            </div>
         </div>
     )
 }
