57257d9276c15b5d5e3eb232f782a54e85b6ac11
yanny63 <contact.yanny0@gmail.com>
2026-05-08 00:50:57 +0200

    save lightMode setting using localStorage, add /login onload animations

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,24 +4,30 @@ import { Route, Routes } from 'react-router-dom'
 import Nav from './components/nav.jsx'
 import Form from './components/form.jsx'
 import Login from "./components/login.jsx"
+import Register from "./components/register.jsx"
 
 
 function App() {
-  const [ lightMode, setLightMode ] = useState(false)
+  const [ lightMode, setLightMode ] = useState(() => {
+    return localStorage.getItem('lightMode') === 'true'
+  })
   const [ search, setSearch ] = useState('')
 
   useEffect(() => {
     document.body.classList.toggle('light', lightMode)
+    console.log(lightMode)
+    localStorage.setItem('lightMode', lightMode)
   }, [lightMode])
   return (
-    <>
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
       <Nav search={search} setSearch={setSearch} lightMode={lightMode} setLightMode={setLightMode} ></Nav>
       <Routes>
         <Route path='/' element={ <Form search={search} setSearch={setSearch} /> } />
-        <Route path='/login' element={ <Login /> } />
+        <Route path='/login' element={ <Login lightMode={lightMode} /> } />
+        <Route path='/register' element={ <Register /> } />
       </Routes>
-    </>
+    </div>
   )
-}
+} 
 
 export default App
--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,7 +1,17 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-function Login() {
+function Login(lightMode) {
+
+    const [ mainLoaded, setMainLoaded ] = useState(false)
+    const [ elementsLoaded, setElementsLoaded ] = useState(false)
+
+    useEffect(() => {
+        setMainLoaded(true)
+        setTimeout(() => {
+            setElementsLoaded(true)
+        }, 400);
+    }, [])
 
     const navigate = useNavigate()
 
@@ -28,12 +38,33 @@ function Login() {
             console.log(`Error - ${err}`)
         }
     }
+
+    function StarLogo(lightMode) {
+        return (
+            <svg width="38" height="38" viewBox="0 0 24 24" fill="none">
+                <path 
+                    d="M12 2L15 9H22L16.5 13.5L18.5 21L12 16.5L5.5 21L7.5 13.5L2 9H9L12 2Z" 
+                />
+            </svg>
+        )
+    }
     return (
-        <div className="login-main">
+        <div className={mainLoaded ? "login-main" : "login-main main-not-loaded"}>
+            <div className="login-form-outer-container">
+                <div className="login-inner-container-left isGradient">
+                    <div style={{ transition: "1s" }} className={ elementsLoaded ? "login-inner-container-logo" : "login-inner-container-logo element-not-loaded-opacity"}> 
+                        <StarLogo lightMode={lightMode}></StarLogo>
+                    </div>
+                    <div className="login-inner-container-isElement">
+                        <h2 style={{ transition: "0.6s" }} className={elementsLoaded ? "" : "content-not-loaded-right element-not-loaded-opacity"}>Contact Manager</h2>
+                        <p style={{ transition: "0.6s" }} className={elementsLoaded ? "" : "content-not-loaded-left element-not-loaded-opacity"}>Wszystko czego potrzebujesz w jednym miejscu.</p>
+                    </div>
+                </div>
+            </div>
             <div className="login-form-outer-container">
                 <div>
-                    <h1>Zaloguj się</h1>
-                    <p>Aby móc czatować ze swoimi znajomymi!</p>
+                    <h1 style={{ transition: "0.6s" }} className={elementsLoaded ? "" : "content-not-loaded-left element-not-loaded-opacity"}>Zaloguj się</h1>
+                    <p style={{ transition: "0.6s" }} className={elementsLoaded ? "" : "content-not-loaded-right element-not-loaded-opacity"}>Aby móc czatować ze swoimi znajomymi!</p>
                 </div>
                 <div className="isLine"></div>
                 <form onSubmit={login} className="login-form">
@@ -47,9 +78,6 @@ function Login() {
                     </div>
                     <button className="form-button">Zaloguj się</button>
                 </form>
-            </div>
-            <div className="login-form-outer-container">
-
             </div>
         </div>
     )
